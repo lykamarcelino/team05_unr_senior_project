dc7e4e40c911610e93518dd71615b8cd1d20a79a
lilipach <45573781+lilipach@users.noreply.github.com>
2020-04-15 00:06:51 -0700

    [FEAT] This works with the the react branch LilianaPacheco/GraphComponents. It adds the graph back end controllers and a template for the Report controllers

--- team05_senior_project/src/main/java/com/intuit/developer/tutorials/controller/logoutController.java
+++ team05_senior_project/src/main/java/com/intuit/developer/tutorials/controller/logoutController.java
@@ -3,23 +3,31 @@
 * */
 package com.intuit.developer.tutorials.controller;
 
-import com.intuit.developer.tutorials.client.OAuth2PlatformClientFactory;
+import org.jfree.data.json.impl.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @CrossOrigin
         (origins = "http://localhost:3000")
 
+@RequestMapping("/logout")
 @Controller
 public class logoutController {
 
-    @RequestMapping("/logout")
-    public void connectToQuickbooks() {
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody
+    JSONObject getLogout()
+    {
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("Response", "Logout endpoint has been hit");
 
         oauthController.accessTokenHolder = null;
         oauthController.realmIdHolder = null;
 
+        return jsonObj;
     }
 
 }
